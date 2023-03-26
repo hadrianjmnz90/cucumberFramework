a7489b73cd0a7bc4a1357e0534c265d3c8b22232
adrianXD <hadrianjmnz90@gmail.com>
2023-03-26 12:10:34 -0600

    added support for edge browser

--- src/test/java/awesomecucumber/factory/DriverFactory.java
+++ src/test/java/awesomecucumber/factory/DriverFactory.java
@@ -3,6 +3,7 @@ package awesomecucumber.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -17,6 +18,10 @@ public class DriverFactory {
             case "firefox" -> {
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
+            }
+            case "edge" -> {
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
             }
             default -> throw new IllegalStateException("INVALID BROWSER: " + browser);
         }
