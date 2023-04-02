936412fb17a5c095dded425a1a9384203b95aeeb
adrianXD <hadrianjmnz90@gmail.com>
2023-04-01 19:54:54 -0600

    options

--- src/test/java/awesomecucumber/factory/DriverFactory.java
+++ src/test/java/awesomecucumber/factory/DriverFactory.java
@@ -3,6 +3,7 @@ package awesomecucumber.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -13,7 +14,9 @@ public class DriverFactory {
         switch (browser) {
             case "chrome" -> {
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(options);
             }
             case "firefox" -> {
                 WebDriverManager.firefoxdriver().setup();
